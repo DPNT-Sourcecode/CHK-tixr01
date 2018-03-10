4fbdbfb2e4530176283fef39eb2e10bf1a9d4dee
sbx_user1059 <sbx_user1059@ip-10-12-8-42.eu-west-2.compute.internal>
2018-03-10 16:26:51 +0000

    Sat Mar 10 16:26:51 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -7,7 +7,29 @@
 def checkout(skus):
     price = 0
 
-    
+    item_A = skus.count('A')
+    item_B = skus.count('B')
+    item_C = skus.count('C')
+    item_D = skus.count('D')
+
+    if len(skus) != (item_A + item_B + item_C +item_D):
+        return -1
+
+    discount = item_A // 3
+    price += discount * 130
+
+    remaining = item_A % 3
+    price += remaining * 50
+
+    discount = item_B // 2
+    price += discount * 45
+
+    remaining = item_B % 2
+    price += remaining * 30
+
+    price += item_C * 20
+
+    price += item_D * 15
 
 
     return price
