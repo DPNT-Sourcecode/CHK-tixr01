630b56e31a87f3e3feb3b297b213874dc84b5307
sbx_user1059 <sbx_user1059@ip-10-12-8-42.eu-west-2.compute.internal>
2018-03-10 15:59:29 +0000

    Sat Mar 10 15:59:29 UTC 2018

--- test/solutions/test_checkout_1.py
+++ test/solutions/test_checkout_1.py
@@ -2,7 +2,7 @@ from lib.solutions.checkout import checkout
 import pytest
 
 
-@pytest.parametrize('test_string, expected_price', [
+@pytest.mark.parametrize('test_string, expected_price', [
 	('2A 2B 3C', 205)
 ])
 def test_checkout(test_string, expected_price):
