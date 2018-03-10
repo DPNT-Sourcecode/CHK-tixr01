c76a55dd4e2527443a2bc3240e52c8d1e531003b
sbx_user1059 <sbx_user1059@ip-10-12-8-42.eu-west-2.compute.internal>
2018-03-10 16:51:35 +0000

    Sat Mar 10 16:51:35 UTC 2018

--- test/solutions/test_checkout_1.py
+++ test/solutions/test_checkout_1.py
@@ -5,7 +5,8 @@ import pytest
 @pytest.mark.parametrize('test_string, expected_price', [
 	('EE', 80),
 	('EEEEBB', 160),
-	('BEBEEE', 160)
+	('BEBEEE', 160),
+	('EE', 80)
 ])
 def test_checkout(test_string, expected_price):
 	assert checkout(test_string) == expected_price
