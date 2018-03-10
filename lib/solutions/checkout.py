5d099f364a0d45187ef69fbbc5051f641766be23
sbx_user1059 <sbx_user1059@ip-10-12-8-42.eu-west-2.compute.internal>
2018-03-10 15:42:21 +0000

    Sat Mar 10 15:42:21 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -2,5 +2,19 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+ITEM_MAP = {
+	'A': 50,
+	'B': 30,
+	'C': 20,
+	'D': 15
+}
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    items = skus.split(' ')
+    cost = 0
+    for item in items:
+    	if item[0] not in ['A', 'B', 'C', 'D']:
+    		return -1
+    	item_type = item[0]
