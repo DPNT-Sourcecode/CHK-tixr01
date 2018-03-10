173f63e231f41abd7614e6b5582c2a3c77306e9a
sbx_user1059 <sbx_user1059@ip-10-12-8-42.eu-west-2.compute.internal>
2018-03-10 15:45:21 +0000

    Sat Mar 10 15:45:21 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -13,8 +13,16 @@ ITEM_MAP = {
 
 def checkout(skus):
     items = skus.split(' ')
-    cost = 0
+    price = 0
     for item in items:
     	if item[0] not in ['A', 'B', 'C', 'D']:
     		return -1
     	item_type = item[0]
+
+    	try:
+    		portion = int(item[1:])
+    	except:
+    		return -1
+
+    	if item_type == 'A':
+
