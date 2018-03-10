5bf7c87a67352e1da227482a586fc419d0bf7746
sbx_user1059 <sbx_user1059@ip-10-12-8-42.eu-west-2.compute.internal>
2018-03-10 15:57:21 +0000

    Sat Mar 10 15:57:21 UTC 2018

--- test/solutions/test_checkout_1.py
+++ test/solutions/test_checkout_1.py
@@ -1,5 +1,9 @@
 from lib.solutions.checkout import checkout
+import pytest
 
 
-def test_checkout('2A 2B 3C'):
-	assert 205
+@pytest.parametrize('test_string, expected_price', [
+	('2A 2B 3C', 205)
+])
+def test_checkout(test_string, expected_price):
+	assert checkout(test_string) == expected_price
