1df4d337ffed8554dd023229ddee250df2c27316
sbx_user1059 <sbx_user1059@ip-10-12-8-42.eu-west-2.compute.internal>
2018-03-10 16:15:58 +0000

    Sat Mar 10 16:15:58 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -10,32 +10,32 @@ def checkout(skus):
 
     for item in items:
         if item not in ['A', 'B', 'C', 'D']:
-            return -1
-        item_type = item[-1]
+            temporary += item
+        else:
+            item_type = item
+            try:
+               portion = int(temporary)
+            except:
+               return -1
 
-        try:
-           portion = int(item[:-1])
-        except:
-           return -1
+            if item_type == 'A':
+                discounted = portion // 3
+                price += discounted * 130
 
-        if item_type == 'A':
-            discounted = portion // 3
-            price += discounted * 130
+                remaining_A = portion % 3
+                price += remaining * 50
 
-            remaining_A = portion % 3
-            price += remaining * 50
+            if item_type == 'B':
+                discounted = portion // 2
+                price += discounted * 45
 
-        if item_type == 'B':
-            discounted = portion // 2
-            price += discounted * 45
+                remaining_B = portion % 2
+                price += remaining * 30
 
-            remaining_B = portion % 2
-            price += remaining * 30
+            if item_type == 'C':
+                price += portion * 20
 
-        if item_type == 'C':
-            price += portion * 20
-
-        if item_type == 'D':
-            price += portion * 15
+            if item_type == 'D':
+                price += portion * 15
 
     return price
